a63a6052d98e1cae9aeec4965b740fabec308437
Mikhail9845 <mihail007.08@mail.ru>
2022-06-08 21:03:11 +0300

    added new file

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ public class Main {
         int amount = 1159;// параметров программы: начального счёта,
         int bonus = amount / 100;
         int sum = account + amount;
-        if (amount >= 1000){
+        if (amount >= 1000) {
             System.out.println("Вас счет =  " + sum);
-            System.out.println("Ваш бонус = " + bonus );
+            System.out.println("Ваш бонус = " + bonus);
+        } else {
+            System.out.println("Вас счет = " + sum);
+            System.out.println("Ваш бонус = 0");
         }
-        else {System.out.println("Вас счет = " + sum );
-        System.out.println("Ваш бонус = 0");}
     }
 }
